d377ecd4ee1c6c8aeece920a91b8705dbfce1267
yaowuya <zrfyaowuya@126.com>
2023-08-17 21:02:55 +0800

    feature: map和string

---
+++ container/map.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string]string{
+		"name":    "ccmouse",
+		"course":  "golang",
+		"site":    "imooc",
+		"quality": "notbad",
+	}
+	m2 := make(map[string]int)
+	var m3 map[string]int
+
+	fmt.Println("m, m2, m3:")
+	fmt.Println(m, m2, m3)
+
+	fmt.Println("Traversing map m")
+	courseName := m["course"]
+	fmt.Println(`m["course"] =`, courseName)
+	if causeName, ok := m["cause"]; ok {
+		fmt.Println(causeName)
+	} else {
+		fmt.Println("key 'cause' does not exist")
+	}
+
+	fmt.Println("Deleting values")
+	name, ok := m["name"]
+	fmt.Printf("m[%q] before delete: %q, %v\n",
+		"name", name, ok)
+
+	delete(m, "name")
+	name, ok = m["name"]
+	fmt.Printf("m[%q] after delete: %q, %v\n",
+		"name", name, ok)
+
+}
