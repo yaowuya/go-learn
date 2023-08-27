2fb6cde7a539a385d444c72249e6583a2474e33f
yaowuya <zrfyaowuya@126.com>
2023-08-27 13:50:37 +0800

    feature: 函数式编程

---
+++ function/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"go-learn/function/fib"
+	"io"
+	"strings"
+)
+
+type intGen func() int
+
+func (g intGen) Read(p []byte) (n int, err error) {
+	next := g()
+	if next > 10000 {
+		return 0, io.EOF
+	}
+	s := fmt.Sprintf("%d\n", next)
+	return strings.NewReader(s).Read(p)
+}
+
+func printFileContents(reader io.Reader) {
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func adder() func(int) int {
+	sum := 0
+	return func(v int) int {
+		sum += v
+		return sum
+	}
+}
+
+type iAdder func(int) (int, iAdder)
+
+func adder2(base int) iAdder {
+	return func(v int) (int, iAdder) {
+		return base + v, adder2(base + v)
+	}
+}
+
+func main() {
+	var f intGen = fib.Fibonacci()
+	printFileContents(f)
+
+	// a := adder() is trivial and also works.
+	a := adder2(0)
+	for i := 0; i < 10; i++ {
+		var s int
+		s, a = a(i)
+		fmt.Printf("0 + 1 + ... + %d = %d\n", i, s)
+	}
+}
